2db7c6608079a2df510823e79c37d7e87ec1f83e
prasamaha <prastamaha@gmail.com>
2025-05-14 17:29:56 +0800

    Brew formula update for awsctl version v0.0.3

--- awsctl.rb
+++ awsctl.rb
@@ -5,11 +5,11 @@
 class Awsctl < Formula
   desc ""
   homepage "https://github.com/prastamaha/awsctl"
-  version "0.0.2"
+  version "0.0.3"
 
   on_macos do
-    url "https://github.com/prastamaha/awsctl/releases/download/v0.0.2/awsctl_0.0.2_darwin_all.tar.gz"
-    sha256 "41a5c9e74915e1fd4d8ee546c5d462c70a84582388d5ee9d8be1172ddba0189b"
+    url "https://github.com/prastamaha/awsctl/releases/download/v0.0.3/awsctl_0.0.3_darwin_all.tar.gz"
+    sha256 "3f69be1ccfdaa9ddbb73dde4a3972ecac30a5d4c7d790b50d8f348f811475b43"
 
     def install
       bin.install "awsctl"
@@ -17,24 +17,18 @@ class Awsctl < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/prastamaha/awsctl/releases/download/v0.0.2/awsctl_0.0.2_linux_amd64.tar.gz"
-        sha256 "b1273bf6da1bbf888983535ee84f7f5d16327258efa812bf2dbd1c99c81f73c4"
-
-        def install
-          bin.install "awsctl"
-        end
+    if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+      url "https://github.com/prastamaha/awsctl/releases/download/v0.0.3/awsctl_0.0.3_linux_amd64.tar.gz"
+      sha256 "55558ce69a82d0ecdfc2d732ac95431e487b30f239f6df62026da0e28da149b2"
+      def install
+        bin.install "awsctl"
       end
     end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/prastamaha/awsctl/releases/download/v0.0.2/awsctl_0.0.2_linux_arm64.tar.gz"
-        sha256 "85128eb22cdc29f0335a6cf2d14652625fbaa370bc071cca6f56b3aa7d48663f"
-
-        def install
-          bin.install "awsctl"
-        end
+    if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+      url "https://github.com/prastamaha/awsctl/releases/download/v0.0.3/awsctl_0.0.3_linux_arm64.tar.gz"
+      sha256 "45ad45f36ffe1206fd46201ed81563f9e229ab2c8653f18eddc6ecb59395a509"
+      def install
+        bin.install "awsctl"
       end
     end
   end
